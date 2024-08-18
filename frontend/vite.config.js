db92c790ca6dfd8ccab5f3da2329ddd532c00832
Dilan Shanuka <99383107+IT21182914@users.noreply.github.com>
2024-08-19 02:02:57 +0530

    Tailwind Setup

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import tailwindcss from "tailwindcss";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 3000,
+  css: {
+    postcss: {
+      plugins: [tailwindcss()],
+    },
   },
 });
