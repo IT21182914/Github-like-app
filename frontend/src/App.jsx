6f11161b65fd79a8b49ba41f4361f618f6ecd2e2
Dilan Shanuka <99383107+IT21182914@users.noreply.github.com>
2024-08-23 08:45:43 +0530

    App

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,28 @@
+import { Routes, Route } from "react-router-dom";
+
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
+import SignUpPage from "./pages/SignUpPage";
+import ExplorePage from "./pages/ExplorePage";
+import LikesPage from "./pages/LikesPage";
+
+import Sidebar from "./components/Sidebar";
+
 function App() {
   return (
-    <div className="text-red-500 bg-blue-400 p-2">Hello from the Home page</div>
+    <div className="flex">
+      <Sidebar />
+
+      <div className="max-w-5xl my-5 text-white mx-auto transition-all duration-300 flex-1">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={<SignUpPage />} />
+          <Route path="/explore" element={<ExplorePage />} />
+          <Route path="/likes" element={<LikesPage />} />
+        </Routes>
+      </div>
+    </div>
   );
 }
 
