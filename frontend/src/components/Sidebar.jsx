4a88b72ccfe284b6a2dd8c789eb797faf9de7bf0
Dilan Shanuka <99383107+IT21182914@users.noreply.github.com>
2024-08-24 02:22:07 +0530

    Complete Frontend

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -7,7 +7,7 @@ import { MdEditDocument } from "react-icons/md";
 import Logout from "./Logout";
 
 const Sidebar = () => {
-  const authUser = false;
+  const authUser = true;
   return (
     <aside
       className="flex flex-col items-center min-w-12 sm:w-16 sticky top-0 left-0 h-screen py-8 overflow-y-auto border-r bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-10 
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import ProfileInfo from "../components/ProfileInfo";
+import Repos from "../components/Repos";
+import Search from "../components/Search";
+import SortRepos from "../components/SortRepos";
 
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <div className="m-4">
+      <Search />
+      <SortRepos />
+      <div className="flex gap-4 flex-col lg:flex-row justify-center items-start">
+        <ProfileInfo />
+        <Repos />
+      </div>
+    </div>
+  );
 };
 
 export default HomePage;
