d9955964fbaa1fc31a9f3e21caae246333480286
Dilan Shanuka <99383107+IT21182914@users.noreply.github.com>
2024-08-27 01:39:51 +0530

    Button Sorted

--- frontend/src/components/SortRepos.jsx
+++ frontend/src/components/SortRepos.jsx
@@ -1,21 +1,24 @@
-const SortRepos = () => {
+const SortRepos = ({ onSort, sortType }) => {
   return (
     <div className="mb-2 flex justify-center lg:justify-end">
       <button
         type="button"
         className={`py-2.5 px-5 me-2 mb-2 text-xs sm:text-sm font-medium focus:outline-none rounded-lg bg-glass`}
+        onClick={() => onSort("recent")}
       >
         Most Recent
       </button>
       <button
         type="button"
         className={`py-2.5 px-5 me-2 mb-2  text-xs sm:text-sm font-medium focus:outline-none rounded-lg bg-glass`}
+        onClick={() => onSort("stars")}
       >
         Most Stars
       </button>
       <button
         type="button"
         className={`py-2.5 px-5 me-2 mb-2  text-xs sm:text-sm font-medium focus:outline-none rounded-lg bg-glass`}
+        onClick={() => onSort("forks")}
       >
         Most Forks
       </button>
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -59,10 +59,22 @@ const HomePage = () => {
     setLoading(false);
   };
 
+  const onSort = (sortType) => {
+    if (sortType === "recent") {
+      repos.sort((a, b) => new Date(b.created_at) - new Date(a.created_at)); //descending, most recent first
+    } else if (sortType === "stars") {
+      repos.sort((a, b) => b.stargazers_count - a.stargazers_count); //descending, most stars first
+    } else if (sortType === "forks") {
+      repos.sort((a, b) => b.forks_count - a.forks_count); //descending, most forks first
+    }
+    setSortType(sortType);
+    setRepos([...repos]);
+  };
+
   return (
     <div className="m-4">
       <Search onSearch={onSearch} />
-      <SortRepos />
+      {repos.length > 0 && <SortRepos onSort={onSort} sortType={sortType} />}
       <div className="flex gap-4 flex-col lg:flex-row justify-center items-start">
         {userProfile && !loading && <ProfileInfo userProfile={userProfile} />}
         {!loading && <Repos repos={repos} />}
