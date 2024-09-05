ef2970aade0478be2c848411cf1589d4bc6e493b
Dilan Shanuka <99383107+IT21182914@users.noreply.github.com>
2024-09-06 03:45:38 +0530

    Auth Routes

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,8 @@ import cors from "cors";
 
 import userRoutes from "./routes/user.route.js";
 import exploreRoutes from "./routes/explore.route.js";
+import authRoutes from "./routes/auth.route.js";
+
 import connectMongoDB from "./db/connectMongoDB.js";
 
 dotenv.config();
@@ -15,6 +17,7 @@ app.get("/", (req, res) => {
   res.send("Server is ready");
 });
 
+app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/explore", exploreRoutes);
 
