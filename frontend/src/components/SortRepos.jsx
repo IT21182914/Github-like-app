53f10951ab78450b14032d6eb4c412ff3b300a8e
Dilan Shanuka <99383107+IT21182914@users.noreply.github.com>
2024-09-07 02:53:59 +0530

    key

--- frontend/src/components/SortRepos.jsx
+++ frontend/src/components/SortRepos.jsx
@@ -8,6 +8,7 @@ const SortRepos = ({ onSort, sortType }) => {
     <div className="mb-2 flex justify-center lg:justify-end">
       {BUTTONS.map((button) => (
         <button
+          key={button.type}
           type="button"
           className={`py-2.5 px-5 me-2 mb-2 text-xs sm:text-sm font-medium focus:outline-none rounded-lg bg-glass
           ${sortType === button.type ? "border-blue-500 text-white" : ""}`}
