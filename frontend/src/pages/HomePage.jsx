d495a5619d3a38a78e8b5777614fc411acfa523c
Dilan Shanuka <99383107+IT21182914@users.noreply.github.com>
2024-09-08 14:06:40 +0530

    URL Change

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -18,9 +18,7 @@ const HomePage = () => {
     async (username = "IT21182914") => {
       setLoading(true);
       try {
-        const res = await fetch(
-          `http://localhost:5000/api/users/profile/${username}`
-        );
+        const res = await fetch(`/api/users/profile/${username}`);
         const { repos, userProfile } = await res.json();
 
         repos.sort((a, b) => new Date(b.created_at) - new Date(a.created_at)); //descending, most recent first
