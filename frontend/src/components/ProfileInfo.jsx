7b78630775dd755c2c9222cf040f4f6e1c16c750
Dilan Shanuka <99383107+IT21182914@users.noreply.github.com>
2024-09-08 10:49:49 +0530

    Add Like Profile

--- frontend/src/components/ProfileInfo.jsx
+++ frontend/src/components/ProfileInfo.jsx
@@ -8,6 +8,7 @@ import { FaXTwitter } from "react-icons/fa6";
 import { TfiThought } from "react-icons/tfi";
 import { FaEye } from "react-icons/fa";
 import { formatMemberSince } from "./utils/functions";
+import LikeProfile from "./LikeProfile";
 import { FaLink } from "react-icons/fa"; // Correct import for FaLink
 
 const ProfileInfo = ({ userProfile }) => {
@@ -43,6 +44,7 @@ const ProfileInfo = ({ userProfile }) => {
           </a>
           {/* View on Github */}
           <div className="flex gap-2 items-center flex-col">
+            <LikeProfile userProfile={userProfile} />
             <a
               href={userProfile?.html_url}
               target="_blank"
