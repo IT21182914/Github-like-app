8b859ee79fddcb8c6b644ff6ca68bf2c19311d5c
Dilan Shanuka <99383107+IT21182914@users.noreply.github.com>
2024-09-08 14:06:22 +0530

    Like Profile

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -24,3 +24,42 @@ export const getUserProfileandRepos = async (req, res) => {
     res.status(500).json({ error: error.message });
   }
 };
+
+export const likeProfile = async (req, res) => {
+  try {
+    const { username } = req.params;
+    const user = await User.findById(req.user._id.toString());
+    console.log(user, "auth user");
+    const userToLike = await User.findOne({ username });
+
+    if (!userToLike) {
+      return res.status(404).json({ error: "User is not a member" });
+    }
+
+    if (user.likedProfiles.includes(userToLike.username)) {
+      return res.status(400).json({ error: "User already liked" });
+    }
+
+    userToLike.likedBy.push({
+      username: user.username,
+      avatarUrl: user.avatarUrl,
+      likedDate: Date.now(),
+    });
+    user.likedProfiles.push(userToLike.username);
+
+    await Promise.all([userToLike.save(), user.save()]);
+
+    res.status(200).json({ message: "User liked successfully" });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+export const getLikes = async (req, res) => {
+  try {
+    const user = await User.findById(req.user._id.toString());
+    res.status(200).json({ likedBy: user.likedBy });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,10 +1,16 @@
 import express from "express";
-import { getUserProfileandRepos } from "../controllers/user.controller.js";
+import {
+  getUserProfileandRepos,
+  likeProfile,
+  getLikes,
+} from "../controllers/user.controller.js";
+import { ensureAuthenticated } from "../middleware/ensureAuthenticated.js";
 
 const router = express.Router();
 
 router.get("/profile/:username", getUserProfileandRepos);
 //TODO => GET likes (who liked our profile)
-//TODO => POST like a profile
+router.get("/likes", ensureAuthenticated, getLikes);
+router.post("/like/:username", ensureAuthenticated, likeProfile);
 
 export default router;
